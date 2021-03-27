a0c33dbc0c7d2bc839686674080b0bcd906b3971
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2021-03-26 21:04:54 -0300

    findById para Categoria

---
+++ src/main/java/com/marcelo/bookstore/service/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.marcelo.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcelo.bookstore.domain.Categoria;
+import com.marcelo.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);
+		
+	}
+	
+}
